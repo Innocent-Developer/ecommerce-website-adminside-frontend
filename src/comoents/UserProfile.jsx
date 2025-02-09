fdc5160b79c5e945689ca1478b2b643c7f696b66
Abubakkar <abubakkarsajid4@gmail.com>
2025-02-09 11:49:40 +0500

    add avator or user edit profile

--- src/comoents/UserProfile.jsx
+++ src/comoents/UserProfile.jsx
@@ -13,10 +13,12 @@ export const UserProfile = () => {
     password: '',
     userImage: ''
   });
+  console.log(USER_ID.id);
+  
 
   useEffect(() => {
-    const userId = USER_ID; // Replace 'USER_ID' with the actual user ID
-    axios.get(`${process.env.REACT_APP_ADMIN_BACKEND_URL}/user-profile/${userId}`)
+    const id = USER_ID; // Replace 'USER_ID' with the actual user ID
+    axios.get(`${process.env.REACT_APP_ADMIN_BACKEND_URL}/user-profile/${id}`)
       .then(response => setProfile(response.data))
       .catch(error => console.error('Error fetching profile:', error));
   }, [USER_ID]);
@@ -31,8 +33,8 @@ export const UserProfile = () => {
   };
 
   const handleSave = () => {
-    const userId = 'USER_ID'; // Replace 'USER_ID' with the actual user ID
-    axios.put(`/api/user-profile/${userId}`, profile)
+    const id =USER_ID; // Replace 'USER_ID' with the actual user ID
+    axios.put(`/user-profile/${id}`, profile)
       .then(() => setIsEditing(false))
       .catch(error => console.error('Error updating profile:', error));
   };
